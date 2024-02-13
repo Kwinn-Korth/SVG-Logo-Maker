1a1a50030d252b342b2a0ac7323d4b77afd5cc10
Kwinn-Korth <kjkorth12@gmail.com>
2024-02-13 12:57:09 -0700

    fixed index.js to properly use inquirer to make the svg and fixed the svg not being made, hopefully

--- index.js
+++ index.js
@@ -1,81 +1,68 @@
-'use strict';
-
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Triangle, Square } = require('./shapes');
+const { Circle, Triangle, Square } = require('./shapes/shapes.js');
 
-async function create_svg(text, text_color, shape, shape_color) {
-  let shapeInstance;
-
-  switch (shape) {
-    case 'circle':
-      shapeInstance = new Circle();
-      break;
-    case 'triangle':
-      shapeInstance = new Triangle();
-      break;
-    case 'square':
-      shapeInstance = new Square();
-      break;
-    default:
-      console.log('Invalid shape choice. Exiting.');
-      process.exit(1);
-  }
-
-  if (shapeInstance) {
-    shapeInstance.setColor(shape_color);
-
-    // Construct SVG content with text and shape
-    const svgContent = `
-      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <text x="150" y="100" fill="${text_color}" text-anchor="middle" alignment-baseline="middle">${text}</text>
-        ${shapeInstance.render()}
-      </svg>
-    `;
-
-    // Write SVG content to file
-    fs.writeFileSync('logo.svg', svgContent);
-
-    console.log('Logo saved as logo.svg');
-  }
-}
-
-async function main() {
-  console.log('Welcome to the Logo Generator');
-
-  const userResponse = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters:',
-      validate: (value) => {
-        if (value.length > 3) {
-          return 'Please enter up to three characters.';
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter 3 characters or less for your logo:',
+            validate: (input) => {
+                if(input.length > 3) {
+                    return 'Only use 3 characters or less.'
+                }
+                return true
+            }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Input a color for the text:'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape for your logo:',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Input a color for the shape:'
         }
-        return true;
-      },
-    },
-    {
-      type: 'input',
-      name: 'text_color',
-      message: 'Enter the text color (color keyword or hexadecimal):',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape:',
-      choices: ['circle', 'triangle', 'square'],
-    },
-    {
-      type: 'input',
-      name: 'shape_color',
-      message: 'Enter the shape color (color keyword or hexadecimal):',
-    },
-  ]);
+    ])
+    .then((data) => {
+    const filename = path.join(examples, `${data.text}.svg`);
 
-  const { text, text_color, shape, shape_color } = userResponse;
+    const logoData = {
+        text: data.text,
+        textColor: data.textColor,
+        shape: data.shape,
+        shapeColor: data.shapeColor
+    }
 
-  create_svg(text, text_color, shape, shape_color);
-}
+    let shapeSvg = ``;
 
-main(); // Call the main function to start the program
+    if (logoData.shape == 'circle') {
+        const circle = new shapes.Circle(150, 100, 80, logoData.shapeColor);
+        shapeSvg = circle.getSvg();
+    } else if (logoData.shape == 'triangle') {
+        const triangle = new shapes.Triangle('150,0 20,200 280,200', logoData.shapeColor);
+        shapeSvg = triangle.getSvg();
+    } else if (logoData.shape == 'square') {
+        const square = new shapes.Square(50, 10, 200, 200, logoData.shapeColor);
+        shapeSvg = square.getSvg();
+    }
+
+    const logoCode = `
+        <svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+     ${shapeSvg}
+     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${logoData.textColor}">${logoData.text}</text>
+    </svg>`;
+    
+    fs.writeFile(filename, logoCode, (err) =>
+    err ? console.log(err) : console.log('Generated Logo!'));
+});
