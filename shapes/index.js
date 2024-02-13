5ffcf1c4450b9b05772b0acfe9407ba61a57f9d7
Kwinn-Korth <kjkorth12@gmail.com>
2024-02-13 12:40:16 -0700

    remade shapes in one file, updated how they are made

--- shapes/index.js
+++
@@ -1,9 +0,0 @@
-const circle = require('./circle');
-const triangle = require('./triangle');
-const square = require('./square');
-
-module.exports = {
-    Circle: circle,
-    Triangle: triangle,
-    Square: square
-    };
---
+++ shapes/shapes.js
@@ -0,0 +1,38 @@
+class Circle {
+    constructor(cy, cx, r, fill) {
+    this.cy = cy;
+    this.cx = cx;
+    this.r = r;
+    this.fill = fill;
+    } getSVG() {
+    return `<circle cx="${this.cx}" cy="${this.cy}" r="${this.r}" fill="${this.fill}" />`;
+    }
+};
+
+class Square {
+    constructor(x, y, width, height, fill) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.fill = fill;
+    } getSVG() {
+    return `<rect x="${this.x}" y="${this.y}" width="${this.width}" height="${this.height}" fill="${this.fill}" />`;
+    }
+};
+
+class Triangle {
+    constructor(x1, y1, x2, y2, x3, y3, fill) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.x3 = x3;
+    this.y3 = y3;
+    this.fill = fill;
+    } getSVG() {
+    return `<polygon points="${this.x1},${this.y1} ${this.x2},${this.y2} ${this.x3},${this.y3}" fill="${this.fill}" />`;
+    }
+};
+
+module.exports = { Circle, Square, Triangle };
